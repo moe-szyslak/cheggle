bbf1ea9273c4fa64989d74005cc37fac947bb38e
Moe Szyslak <moe.duffdude@gmail.com>
2016-06-25 16:05:14 +0300

    bootting
    ...as the joke goes, I'm now ready to get started with React

---
+++ README.md
@@ -0,0 +1,5 @@
+### Checkbox + Toggle = Cheggle™
+
+React Component that displays a checkbox or a toggle depending on the platform.
+
+*Most* of Cheggle styling taken from ionic framework with minor tweaks.
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Cheggle</h1>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('app'));
